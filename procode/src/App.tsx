7fe979210a3fac97aee0ef37ee7e470eeecc011d
Siddarth <siddarth2801@gmail.com>
2023-06-03 19:34:09 +0530

    added textarea for testing

--- procode/src/App.tsx
+++ procode/src/App.tsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
+
 function App() {
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const handleClick = () => {
+        console.log("click");
+    };
+
+
     return (
-        <div>App</div>
+        <div>
+            <textarea onChange={e => setInput(e.target.value)} value={input}></textarea>
+            <div>
+                <button onClick={handleClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
     );
 }
 
