ec45d8e5b699fc563f77824e822ebd4af8e358b0
Siddarth <siddarth2801@gmail.com>
2023-06-03 20:18:07 +0530

    transpiling done

--- procode/src/App.tsx
+++ procode/src/App.tsx
@@ -1,23 +1,32 @@
-import { useState, useEffect } from 'react';
-import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from 'react';
+import * as esbuild from 'esbuild-wasm'; // use * when exporting or importing everything from a file.
 
 function App() {
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
+    const ref = useRef<any>(); // for reference
 
-    const handleClick = () => {
-        console.log("click");
+    const handleClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+        // console.log(ref.current);
+        const result = await ref.current.transform(input, {
+            loader: 'jsx',
+            target: 'es2015'
+        });
+        setCode(result.code);
     };
 
     const startService = async () => {
-        const service = await esbuild.startService({
+        ref.current = await esbuild.startService({
             worker: true,
             wasmURL: './esbuild.wasm'
         });
         // console.log(service);
     };
 
-    // Bs ek baar run kr na bhai
+    // Bs ek baar run krna hai
     useEffect(() => {
         startService();
     }, []);
