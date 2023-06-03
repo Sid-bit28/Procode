59a6e6ed53cbe9dd2c72a4f187d8c42b630daac8
Siddarth <siddarth2801@gmail.com>
2023-06-03 19:25:33 +0530

    added esbuild

--- pages/problems.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Guides.module.css'
-
-export default function Guides() {
-  return (
-    <div className={styles.guides}>
-      <h2>All Problems</h2>
-    </div> 
-  )
-}
