fc84fa64d6754955168a8da98ae3d7d2a4c7b285
Siddarth <siddarth2801@gmail.com>
2023-06-06 16:56:40 +0530

    logo addedgit add .

--- procode/src/App.js
+++ procode/src/App.js
@@ -5,6 +5,13 @@ import CellList from './components/cell-list';
 function App() {
     return (
         <div>
+            <div className='columns'>
+                <div className="column is-12">
+                    <div className='is-flex is-flex-direction-row'>
+                        <h4 className='mr-1 has-text-light is-size-2 is-family-code has-text-weight-bold'> PROCODE </h4>
+                    </div>
+                </div>
+            </div>
             <CellList />
         </div>
     );
